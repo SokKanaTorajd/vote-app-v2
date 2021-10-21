447504e254f4a848464b9eacc92823c0a8139ecc
akhfzz <akhmadfaizal13@gmail.com>
2021-10-21 15:52:21 +0700

    'back'

--- backend/apiHandle.py
+++ backend/apiHandle.py
@@ -166,25 +166,25 @@ class InputFile(Resource):
         file = data['file']
         organisasi = Organisasi.query.filter_by(nm_organisasi=nm_organisasi).first()
 
-        def find_file(basedir, filename):
-            for dirname, dirs, files in os.walk(basedir):
-                if filename in files:
-                    yield(os.path.join(dirname, filename))
-        # try:
-        z = [flex for flex in find_file(os.path.expanduser('~/Documents'), file)]
-        if len(z) == 0:
-            return jsonify({'error': 'Received documents folder'})
+        # def find_file(basedir, filename):
+        #     for dirname, dirs, files in os.walk(basedir):
+        #         if filename in files:
+        #             yield(os.path.join(dirname, filename))
+        # # try:
+        # z = [flex for flex in find_file(os.path.expanduser('~/Documents'), file)]
+        # if len(z) == 0:
+        #     return jsonify({'error': 'Received documents folder'})
         text = file.split('.')
         # print(z)
         engine = create_engine(f"mysql+mysqlconnector://{user}:{password}@{host}/{database}")
         for x in text:
             if x == 'csv':
-                df = pd.read_csv(z[0])
+                df = pd.read_csv(file)
                 df['access_token'] = df['Nama'].apply(lambda _: str(uuid.uuid4()))
                 df.to_sql(organisasi.nm_organisasi,con=engine, if_exists='replace')
                 return jsonify({'file' : f'{file} success uploaded, Sample data will be up for you'})
             elif x == 'xlsx':
-                df = pd.read_excel(z[0])
+                df = pd.read_excel(file)
                 df['access_token'] = df['Nama'].apply(lambda _: str(uuid.uuid4()))
                 df.to_sql(organisasi.nm_organisasi,con=engine, if_exists='replace')
                 # print('excel')
